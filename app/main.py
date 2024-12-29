3d33d04cca64b3fcb4b2b6a665df8e5d1e641a81
jeid12 <niyokwizerajd123@gmail.com>
2024-12-29 23:16:39 +0200

    last change

--- app/main.py
+++ app/main.py
@@ -47,10 +47,9 @@ def shutdown_event():
     print("App is shutting down...")
 
 if __name__ == "__main__":
-    import uvicorn
-     #uvicorn.run(app, host="127.0.0.1", port=8001)
-    
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    # Get the port from the environment variable, default to 8000 if not set
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 
